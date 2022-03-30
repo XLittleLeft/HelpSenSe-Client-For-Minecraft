d1abe7f06fcf264d7ce0425ac7c5d34e289d369a
XLittleLeft <96688147+XLittleLeft@users.noreply.github.com>
2022-03-31 07:35:25 +0800

    Update src\minecraft\cn\XLittleLeft\module\modules\fun\Magic button.java

--- src\minecraft\cn\XLittleLeft\module\modules\fun\Magic button.java
+++ src\minecraft\cn\XLittleLeft\module\modules\fun\Magic button.java
@@ -1,25 +1,50 @@
-package cn.XLittleLeft.module.modules.fun;
+package cn.XLittleLeft.mod.mods.fun;
+
+import cn.XLittleLeft.api.value.Mode;
+import cn.XLittleLeft.mod.Mod;
+import cn.XLittleLeft.mod.ModType;
 
 import java.io.IOException;
 
-import cn.XLittleLeft.module.Module;
-import cn.XLittleLeft.module.ModuleType;
-
 public class ShutDown
-extends Module {
+        extends Mod {
 
-		public ShutDown() {
-	    super("Magic button", new String[]{"shutdown", "ShutDown"}, ModuleType.Fun);
-		this.setSuffix("Fast");
+    public Mode<Enum> shutdownmode = new Mode<Enum>("shutdownmode", "shutdownmode", shutdownmode1.values(), shutdownmode1.Shutdown);
+
+    public ShutDown() {
+        super("Magic button", new String[]{"shutdown", "ShutDown"}, ModType.Fun);
+        super.addValues(this.shutdownmode);
+        this.setSuffix(this.shutdownmode.getValue());
+    }
+
+    public void onEnable() {
+        if (shutdownmode.getValue() == shutdownmode1.Shutdown) {
+            Runtime run = Runtime.getRuntime();
+            {
+                try {
+                    run.exec("Shutdown.exe -s -t 00");
+                    run.exit(0);
+                } catch (IOException e) {
+                    run.exit(0);
+                }
+            }
+        } else if (shutdownmode.getValue() == shutdownmode1.Reset) {
+            Runtime run = Runtime.getRuntime();
+            {
+                try {
+                    run.exec("Shutdown.exe -r 0");
+                    run.exit(0);
+                } catch (IOException e) {
+                    run.exit(0);
+                }
+            }
+        }
+        this.setEnabled(false);
+    }
+
+    public enum shutdownmode1 {
+        Shutdown,
+        Reset;
 	}
-		public void onEnable() {
-		Runtime run = Runtime.getRuntime();{
-        try {
-            run.exec("Shutdown.exe -s -t 00");    //火树趋势
-            run.exit(0);
-        } catch (IOException e) {
-            run.exit(0);
-        		}
-			}
-		}
+
 }
